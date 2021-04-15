b8dc02a893ad40c37ebcd67fd90139c53cff57e7
Tom Pan <tom_pan@seu.edu.cn>
2021-04-15 00:48:41 -0800

    1

--- 1004.cpp
+++ 1004.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-void sort(int*, int, int, int);
+void sort(int *, int, int, int);
 int main()
 {
     int data[100][1000];
     int m;
     cin >> m;
-    for (int i = 0;i < m;i++)
+    for (int i = 0; i < m; i++)
     {
         cin >> data[i][0];
-        for (int j = 1;j <= data[i][0];j++)
+        for (int j = 1; j <= data[i][0]; j++)
         {
             cin >> data[i][j];
         }
-
     }
 }
-void sort(int* data, int head, int tail, int level)
+void sort(int *data, int head, int tail, int level)
 {
     if (head >= tail)
         return;
-    if(level==3)
+    if (level == 3)
     {
-        
+        for (int i = 0; i < tail; i++)
+        {
+            cout << data[i] << endl;
+        }
     }
 }
